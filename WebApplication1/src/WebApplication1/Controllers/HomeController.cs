ab44fed24a6ef560bf7a8c6ad55cf809cab3cfc5
Simone Chiaretta <simone@piyosailing.com>
2016-06-07 22:43:16 +0200

    Added sample app

---
+++ WebApplication1/src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
