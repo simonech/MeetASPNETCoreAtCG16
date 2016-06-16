e8f4ec10ac379b484eaf8027e8423cd33f6673c2
Simone Chiaretta <simone@piyosailing.com>
2016-06-16 22:30:46 +0200

    Added complete demo app

--- ASPNETCore App/src/WebApplication1/Controllers/HomeController.cs
+++ ASPNETCore App/src/WebApplication1/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebApplication1.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
@@ -10,6 +9,17 @@ namespace WebApplication1.Controllers
     public class HomeController: Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Throw()
+        {
+            throw new Exception("Oh snap!!");
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
--- ASPNETCore App/src/WebApplication1/Startup.cs
+++ ASPNETCore App/src/WebApplication1/Startup.cs
@@ -7,12 +7,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
     public class Startup
     {
 
+        public IConfigurationRoot Configuration { get; }
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -23,9 +25,6 @@ namespace WebApplication1
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -34,14 +33,22 @@ namespace WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app
+            , IHostingEnvironment env
+            , ILoggerFactory loggerFactory
+            )
         {
+
+            var logger = loggerFactory.CreateLogger("Info");
+            loggerFactory.AddConsole();
+            logger.LogInformation("Process started");
+
+
             app.UseStatusCodePages();
-            
-            //app.UseStaticFiles();
 
             if (env.IsDevelopment())
             {
+                app.UseRuntimeInfoPage();
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -49,11 +56,13 @@ namespace WebApplication1
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseStaticFiles();
 
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}");
+            app.UseMvcWithDefaultRoute();
+            app.Run(async (context) =>
+            {
+                //await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(String.Format("{0} {1}!", Configuration["Salutation:Word"], Configuration["Salutation:ToWho"]));
             });
         }
     }
